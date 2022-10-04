6a6e0c5731cf525ed2af8e6c5efdb4f1ec3d6770
Divyashrimb <113498321+Divyashrimb@users.noreply.github.com>
2022-10-04 11:10:31 +0530

    Add files via upload

---
+++ LinkedTreeSet.java
@@ -0,0 +1,13 @@
+import java.util.TreeSet;
+
+public class LinkedTreeSet {
+    public static void main(String[] args) {
+        TreeSet<String> t = new TreeSet<>();
+        t.add("Divya");
+        t.add("Ayesha");
+        t.add("Imran");
+        t.add("Divya");
+        System.out.println(t);
+    }
+}
+

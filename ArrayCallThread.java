6ac5229766bb974900c948904f6eff578854efcc
Divyashrimb <113498321+Divyashrimb@users.noreply.github.com>
2022-10-11 14:45:57 +0530

    Add files via upload

---
+++ ArrayCallThread.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.*;
+
+class Tas1 implements Callable<Integer> {
+
+    public Integer call() throws Exception {
+        Thread.sleep(1000);
+        return new Random().nextInt();
+    }
+}
+ class ArrayCallThread {
+     public static void main(String[] args) throws ExecutionException, InterruptedException {
+         ExecutorService service = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+         List<Future> l = new ArrayList<>();
+
+
+         for (int i = 0; i < 10; i++) {
+             Future<Integer> future = service.submit(new Task1());
+             future.get();//holds the place not the value
+             System.out.println(future.get());
+             l.add(future);
+         }
+         for (Future l1:l) {//l is the object created in arraylist
+             System.out.println(l1.get());
+
+         }
+     }
+ }
